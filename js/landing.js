78d0757d592d676b6e1a94c99e077733e21cc32d
Daniel sanchez <jdaniel.saed@gmail.com>
2016-06-21 13:54:54 +0000

    fix map onclick redirect url

--- js/landing.js
+++ js/landing.js
@@ -3,32 +3,32 @@
   // document ready  
   $(function() {
     var baseUrl = $('#baseUrl').text();
-        
-    $('#search-form').on('submit', function(e) {
-      e.preventDefault();
-      var search = $('#search').val();
-      window.location = baseUrl + '/initiatives?q='+search;
-    });
-    
+    var $region = $('.region');
     var typedConfig = {
       strings: [''],
       typeSpeed: 20,
       loop: false
     }
 
-    $('#blinking').typed(typedConfig);
-
-    $('.region').on('mouseenter', function() {
-      var name = $(this).prop('id').replace(/-+/g, ' '); // replaces '-' with ' '
-      typedConfig.strings = [name];
-      $('#blinking').typed('reset');
-      $('#blinking').typed(typedConfig);
+    $('#search-form').on('submit', function(e) {
+      e.preventDefault();
+      var search = $('#search').val();
+      window.location = baseUrl + '/database?q='+search;
     });
 
-    $('.region').on('click', function() {
-      var name = $(this).prop('id').replace(/-+/g, ' ');;
-      window.location = baseUrl + '/initiatives?q='+name;
-    });    
+    $('#blinking').typed(typedConfig);
+
+    $region
+      .on('mouseenter', function() {
+        var name = $(this).prop('id').replace(/-+/g, ' '); // replaces '-' with ' '
+        typedConfig.strings = [name];
+        $('#blinking').typed('reset');
+        $('#blinking').typed(typedConfig);
+      })
+      .on('click', function() {
+        var name = $(this).prop('id').replace(/-+/g, ' ');;
+        window.location = baseUrl + '/database?q='+name;
+      });    
   });
 
 })($)
