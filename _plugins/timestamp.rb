af956efbb6b75f79daba1ce72190c2aa92985f7d
Mac Cowell <macowell@gmail.com>
2019-10-21 04:44:17 -0700

    dirty hack to make jekyll think everything was just modified

--- _plugins/timestamp.rb
+++ _plugins/timestamp.rb
@@ -10,11 +10,9 @@ module LastModified
     		if(collection.respond_to? :data)
           last_modified = `git log -1 --format="%ct" -- "#{collection.path}"`
       		last_modified.strip!
-      #    dateISO = Date.strptime(last_modified, '%a %b %d %H:%M:%S %y %z')
-          collection.data["timestamp"] = last_modified
-      #    require "time"
-      #    dateToStamp = Date.(dateISO).to_time.to_i
-      #		collection.data["timestamp"] = dateToStamp
+          #collection.data["timestamp"] = last_modified
+          # dirty hack to make jekyll think everything was just modified 
+          collection.data["timestamp"] = 1571657847
     		end
     	end
     end
